8c2c4adcc4a929e79190c05bf8a21523b5b9bdc7
unknown <yunusahmad0022@gmail.com>
2023-11-16 18:28:25 +0800

    add all

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Nama image Docker yang akan dibuat
-IMAGE_NAME="ghcr.io/SiEggo/a433-microservices/item-app:v1"
-
 # Membuat Docker image
-docker build -t $IMAGE_NAME .
+docker build -t item-app:v1 .
 
 # Melihat daftar image di lokal
 docker images
 
 # Login ke GitHub Container Registry (GHCR) dengan menggunakan Personal Access Token (PAT)
-docker login ghcr.io -u SiEggo -p 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
+docker login ghcr.io -u sieggo -p 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
 
 # Mengganti nama image agar sesuai dengan format GHCR
-docker tag $IMAGE_NAME $IMAGE_NAME
+docker tag item-app:v1 ghcr.io/sieggo/a433-microservices/item-app:v1
 
 # Mengunggah image ke GitHub Container Registry
-docker push $IMAGE_NAME
+docker push ghcr.io/sieggo/a433-microservices/item-app:v1
